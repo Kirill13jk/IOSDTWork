4302277289a29f6753e1b8a573791c89ac77f8bc
Kirill <onlyjesus13@icloud.com>
2024-09-07 17:57:51 +0500

    iosdt commit 3

---
+++ IOSDTWork/ChangePasswordViewController.swift
@@ -0,0 +1,80 @@
+import UIKit
+import KeychainSwift
+
+class ChangePasswordViewController: UIViewController {
+    
+    let newPasswordTextField = UITextField()
+    let confirmPasswordTextField = UITextField()
+    let saveButton = UIButton(type: .system)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Изменить пароль"
+        
+        setupUI()
+    }
+    
+    private func setupUI() {
+        newPasswordTextField.placeholder = "Новый пароль"
+        newPasswordTextField.isSecureTextEntry = true
+        newPasswordTextField.borderStyle = .none
+        newPasswordTextField.layer.cornerRadius = 8
+        newPasswordTextField.layer.borderWidth = 1
+        newPasswordTextField.layer.borderColor = UIColor.systemGray4.cgColor
+        newPasswordTextField.setLeftPaddingPoints(10)
+        
+        confirmPasswordTextField.placeholder = "Подтвердите пароль"
+        confirmPasswordTextField.isSecureTextEntry = true
+        confirmPasswordTextField.borderStyle = .none
+        confirmPasswordTextField.layer.cornerRadius = 8
+        confirmPasswordTextField.layer.borderWidth = 1
+        confirmPasswordTextField.layer.borderColor = UIColor.systemGray4.cgColor
+        confirmPasswordTextField.setLeftPaddingPoints(10)
+        
+        saveButton.setTitle("Сохранить", for: .normal)
+        saveButton.addTarget(self, action: #selector(savePassword), for: .touchUpInside)
+        
+        let stackView = UIStackView(arrangedSubviews: [newPasswordTextField, confirmPasswordTextField, saveButton])
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+        
+        newPasswordTextField.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        confirmPasswordTextField.heightAnchor.constraint(equalToConstant: 40).isActive = true
+    }
+    
+    @objc private func savePassword() {
+        guard let newPassword = newPasswordTextField.text, let confirmPassword = confirmPasswordTextField.text, !newPassword.isEmpty, newPassword == confirmPassword else {
+            showError("Пароли не совпадают")
+            return
+        }
+        
+        let keychain = KeychainSwift()
+        keychain.set(newPassword, forKey: "userPassword")
+        dismiss(animated: true, completion: nil)
+    }
+    
+    private func showError(_ message: String) {
+        let alert = UIAlertController(title: "Ошибка", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "ОК", style: .default))
+        present(alert, animated: true, completion: nil)
+    }
+}
+
+// Extension для добавления отступов в UITextField
+extension UITextField {
+    func setLeftPaddingPoints(_ amount:CGFloat){
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+}
--- IOSDTWork/SettingsViewController.swift
+++ IOSDTWork/SettingsViewController.swift
@@ -1,19 +1,61 @@
 import UIKit
 
-class SettingsViewController: UIViewController {
-    let settingsLabel = UILabel()
-
+class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private var tableView: UITableView!
+    private let options = ["Сортировка файлов", "Изменить пароль"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Настройки"
         view.backgroundColor = .white
         
-        settingsLabel.text = "Настройки"
-        settingsLabel.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(settingsLabel)
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        tableView = UITableView(frame: view.bounds, style: .grouped)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        view.addSubview(tableView)
+    }
+    
+    // MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return options.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = options[indexPath.row]
         
-        NSLayoutConstraint.activate([
-            settingsLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            settingsLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
+        if indexPath.row == 0 {
+            // Добавляем UISwitch для сортировки
+            let sortSwitch = UISwitch(frame: .zero)
+            sortSwitch.setOn(UserDefaults.standard.bool(forKey: "sortOrder"), animated: true)
+            sortSwitch.addTarget(self, action: #selector(switchChanged(_:)), for: .valueChanged)
+            cell.accessoryView = sortSwitch
+        }
+        
+        return cell
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 1 {
+            // Открываем экран для изменения пароля
+            let changePasswordVC = ChangePasswordViewController()
+            changePasswordVC.modalPresentationStyle = .formSheet
+            present(changePasswordVC, animated: true, completion: nil)
+        }
+    }
+    
+    @objc func switchChanged(_ sender: UISwitch) {
+        // Сохраняем сортировку в UserDefaults
+        UserDefaults.standard.set(sender.isOn, forKey: "sortOrder")
+        UserDefaults.standard.synchronize() // Применяем изменения
     }
 }
